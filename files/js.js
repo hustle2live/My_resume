16e9a92cb3ee46ce348027187ef42266ddc664f9
hustle2live <87603877+hustle2live@users.noreply.github.com>
2021-11-13 15:27:10 +0200

    moderate with js

---
+++ files/js.js
@@ -0,0 +1,11 @@
+document
+  .querySelector('.button-first')
+  .addEventListener('click', () => document.querySelector('.first-list').classList.toggle('hide'));
+
+document
+  .querySelector('.button-second')
+  .addEventListener('click', () => document.querySelector('.second-list').classList.toggle('hide'));
+
+document
+  .querySelector('.button-third')
+  .addEventListener('click', () => document.querySelector('.third-list').classList.toggle('hide'));
